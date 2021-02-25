7973399b165a7cd1cddc66c90616f0ca28f7d312
HayatoMoromasa <bboy.hayato1tribal@gmail.com>
2021-02-26 07:54:25 +0900

    add style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import styled from 'styled-components';
 import { useEffect,useState } from 'react';
 import { List } from "./List";
 import { Form } from "./Form";
 import { getLanguages } from './const/languages';
+
+
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 30px;
+  border-bottom: 1px solid #e0e0e0;
+`
+const HeaderUl = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+`
+const HeaderLi = styled.li`
+  list-style: none;
+  padding: 4px 12px;
+  cursor: pointer;
+  border-bottom: ${ props => props.focused ? '2px solid #f44336' : 'none'};
+`
 
 
 function App() {
@@ -27,12 +48,12 @@ function App() {
 
   return (
     <div>
-    <header>
-      <ul>
-        <li onClick={() => setTab('list')}>List</li>
-        <li onClick={() => setTab('form')}>Form</li>
-      </ul>
-    </header>
+    <Header>
+      <HeaderUl>
+        <HeaderLi focused={tab === 'list'} onClick={() => setTab('list')}>List</HeaderLi>
+        <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>Form</HeaderLi>
+      </HeaderUl>
+    </Header>
       {
         tab === 'list' ?  <List langs={langs}/> : <Form onAddLang={addLang}/>
       }
